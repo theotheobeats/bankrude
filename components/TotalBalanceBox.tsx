aadd251203909833708c913c81967b646bd14861
Theo <asakvsa.idn@gmail.com>
2024-06-24 16:29:02 +0700

    app finishing

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -14,7 +14,7 @@ const TotalBalanceBox = ({
         <div className="flex flex-col gap-2">
           <p className="total-balance-label">Total Current Balance</p>
           <div className="total-balance-amount flex-center gap-2">
-            <AnimatedCounter amount={1250}/>
+            <AnimatedCounter amount={totalCurrentBalance}/>
           </div>
         </div>
       </div>
